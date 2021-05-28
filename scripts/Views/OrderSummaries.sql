5da0f26c82af0ed68f4317c1ac24933e1c580015
MetapodOP <82407090+the-metapod@users.noreply.github.com>
2021-05-28 23:45:55 +0200

    Fix OrderSummaries view

--- scripts/Views/OrderSummaries.sql
+++ scripts/Views/OrderSummaries.sql
@@ -1,7 +1,7 @@
 USE PBD_lab
 GO
 CREATE VIEW OrderSummaries AS
-SELECT 
+SELECT
 orders.OrderId,
 orders.CustomerID,
 PlacementDateTime,
@@ -10,9 +10,9 @@ SUM(Quantity) AS TotalQuantity,
 SUM(TotalGross) AS AbsoluteGross,
 SUM(TotalNet) AS AbsoluteNet,
 discounts.DiscountPercentage AS DiscountPercentage,
-dbo.apply_discount(AbsoluteGross, DiscountPercentage) AS TotalGross,
-dbo.apply_discount(AbsoluteNet, DiscountPercentage) AS TotalNet
+dbo.apply_discount(SUM(TotalGross), DiscountPercentage) AS TotalGross,
+dbo.apply_discount(SUM(TotalNet), DiscountPercentage) AS TotalNet
 FROM Orders orders
 LEFT JOIN OrderEntrySummaries entries ON orders.OrderID = entries.OrderID
 LEFT JOIN Discounts discounts ON orders.DiscountID = discounts.DiscountID
-GROUP BY orders.OrderID, orders.CustomerID, discounts.DiscountPercentage
+GROUP BY orders.OrderID, orders.CustomerID, orders.PlacementDateTime, orders.RealizationDateTime, discounts.DiscountPercentage
