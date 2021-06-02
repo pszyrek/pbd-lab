913812070828e31dc9ecc9fc5a13cb622a4c3e27
MetapodOP <82407090+the-metapod@users.noreply.github.com>
2021-06-02 21:57:49 +0200

    FIX: discount function

--- scripts/Functions/applyDiscount.sql
+++ scripts/Functions/applyDiscount.sql
@@ -1,7 +1,7 @@
 USE PBD_lab
 GO
-CREATE FUNCTION apply_discount(@original_price DECIMAL(5,2), @discount_percentage DECIMAL(2,2))
-RETURNS DECIMAL(5,2)
+CREATE FUNCTION apply_discount(@original_price DECIMAL(6,2), @discount_percentage DECIMAL(2,2))
+RETURNS DECIMAL(6,2)
 BEGIN
 	IF(@discount_percentage = NULL)
 		RETURN @original_price
